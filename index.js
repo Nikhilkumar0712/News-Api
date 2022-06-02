86d873f912dbc653235527f756213b8d815fd6ba
nikhilphotos0712 <Nikhilphotos0712@gmail.com>
2022-06-02 12:13:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+let news = document.getElementById("news")
+const xhr = new XMLHttpRequest()
+xhr.open("GET", 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=2c7761913bed4cabb16cca0976fbaac2', true)
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles
+        let newsHtml=''
+       articles.forEach(element => {
+            let news = 
+            `<div class="card" id="news">
+            <h4>${element['title']}</h4>
+            <img src="${element['urlToImage']}" alt="Avatar">
+            <div class="container">
+              <h4><b>${element['description']}</b></h4>
+              <p>${element['content']}</p>
+              <br>
+              View Full Story here:<a target="_blank" href="${element['url']}">${element['url']}</a>    
+            </div>
+          </div>`
+            
+        
+            newsHtml += news
+        });  
+        news.innerHTML = newsHtml
+    }
+    else {
+        console.log("some error ");
+    }
+}
+
+xhr.send();
+
+
+
